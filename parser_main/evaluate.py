f1cddf53b6fc2da3d519619094f99f83f5b40968
tkayadelen <kayadelen.tolga@gmail.com>
2019-08-11 23:20:04 +0100

    Implements typed precision, recall and f1 metrics for labeled attachment score, #12

--- parser_main/evaluate.py
+++ parser_main/evaluate.py
@@ -11,6 +11,7 @@ We provide typed based precision, recall and F1 scores for LAS and type based
 accuracy for UAS.
 
 """
+import pandas as pd
 from data.treebank import sentence_pb2
 from data.treebank import treebank_pb2
 from util import common
@@ -33,6 +34,11 @@ def evaluate_parser(args):
 	#evaluator = Evaluator(gold_data, test_data)
 	#evaluator.Evaluate(eval_metrics)
 
+get_labels = lambda sent: [token.label for token in sent.token[1:]]
+
+same_head = (lambda t1, t2:
+	t1.selected_head.address == t2.selected_head.address)
+
 class Evaluator:
 
 	def __init__(self, gold, test):
@@ -47,19 +53,22 @@ class Evaluator:
 		assert isinstance(test, treebank_pb2.Treebank)
 		self.gold = list(gold.sentence)
 		self.test = list(test.sentence)
+		self.gold_and_test = zip(self.gold, self.test)
 		self.uas_total = 0.0
 		self.las_total = 0.0
 		self.typed_uas = {}
 		self.typed_las_prec = {}
 		self.typed_las_recall = {}
-		self.typed_las_f1 = {}
+		self.typed_las_f1 = None
 
 	def _UasTotal(self):
 		"Computes the total Unlabeled Attachement Score of the parser."
 		uas = 0.0
-		for gold_sent, test_sent in zip(self.gold, self.test):
-			gold_heads = [token.selected_head.address for token in gold_sent.token[1:]]
-			pred_heads = [token.selected_head.address for token in test_sent.token[1:]]
+		for gold_sent, test_sent in self.gold_and_test:
+			gold_heads = [token.selected_head.address for token in
+			 gold_sent.token[1:]]
+			pred_heads = [token.selected_head.address for token in
+			 test_sent.token[1:]]
 			assert len(gold_heads) == len(pred_heads), "Tokenization mismatch!!"
 			#for gold_head, predicted_head in zip(gold_heads, pred_heads):
 			#	print("gold: {}, predicted: {}, equal: {}".format(
@@ -68,13 +77,11 @@ class Evaluator:
 			uas += 100 * sum(
 					gh == ph for gh, ph in zip(gold_heads, pred_heads)) / len(gold_heads)
 		self.uas_total = uas / len(self.gold)
-		return self.uas_total
-
 
 	def _LasTotal(self):
 		"Computes the total Labeled Attachment Score of the parser."
 		las = 0.0
-		for gold_sent, test_sent in zip(self.gold, self.test):
+		for gold_sent, test_sent in self.gold_and_test:
 			gold_heads = [(token.selected_head.address, token.label)
 			 for token in gold_sent.token[1:]]
 			pred_heads = [(token.selected_head.address, token.label)
@@ -86,23 +93,83 @@ class Evaluator:
 			las += 100 * sum(
 				gh == ph for gh, ph in zip(gold_heads, pred_heads)) / len(gold_heads)
 		self.las_total = las / len(self.gold)
-		return self.las_total
 
 	def _TypedUas(self):
 		"Computes Unlabeled Attachment Score for all dependency types."
 		pass
 
 	def	_TypedLasPrec(self):
-		"Computes Precision for all dependency types."
-		pass
+		"""Computes Precision for all dependency types.
+
+		For each relation X, precision computes the percentage of relations X
+		in the system that are correct (correct / system)
+		"""
+		labels = list(set().union(*map(get_labels, self.test)))
+		for label in labels:
+			#print("Computing precision for {}".format(label))
+			correct = 0.0
+			system = 0.0
+			match = []
+			sentence_idx = 0
+			for gold_sent, test_sent in self.gold_and_test:
+				sentence_idx += 1
+				for gold_tok, test_tok in zip(gold_sent.token, test_sent.token):
+					if not test_tok.label == label:
+						continue
+					system += 1
+					if gold_tok.label == label and same_head(gold_tok, test_tok):
+						correct += 1
+						match.append((sentence_idx, gold_tok.index, gold_tok.word))
+			#logging.info("System has: {} \"{}\"".format(system, label))
+			#logging.info("{} of the system token(s) were also \"{}\" in gold".format(correct, label))
+			#logging.info("Precision of the model for label \"{}\" is {} / {} = {}".format(
+			#	label, correct, system, correct / system)
+			#)
+			#logging.info("matches are: {}".format(match))
+			self.typed_las_prec[label] = round((correct / system), 2)
+			#print("----------------------------------------------------------")
 
 	def _TypedLasRecall(self):
 		"Computes Recall for all dependency types."
-		pass
+		labels = list(set().union(*map(get_labels, self.gold)))
+		for label in labels:
+			#print("Computing recall for {}".format(label))
+			correct = 0.0
+			gold = 0.0
+			match = []
+			sentence_idx = 0
+			for gold_sent, test_sent in self.gold_and_test:
+				sentence_idx += 1
+				for gold_tok, test_tok in zip(gold_sent.token, test_sent.token):
+					if not gold_tok.label == label:
+						continue
+					gold += 1
+					if test_tok.label == label and same_head(gold_tok, test_tok):
+						correct += 1
+						match.append((sentence_idx, test_tok.index, test_tok.word))
+			#logging.info("Gold has: {} \"{}\"".format(gold, label))
+			#logging.info("{} of the gold label(s) were recovered by system".format(correct))
+			#logging.info("Recall of the model for label \"{}\" is {} / {} = {}".format(
+			#	label, correct, gold, correct / gold)
+			#)
+			#logging.info("matches are: {}".format(match))
+			self.typed_las_recall[label] = round((correct / gold), 2)
+			#print("----------------------------------------------------------")
 
 	def _TypedLasF1(self):
 		"Computes F1 score for all dependency types."
-		pass
+		#F1 Score = 2*(Recall * Precision) / (Recall + Precision)
+		f1 = lambda recall, precision: 2 * (recall * precision) / (recall + precision)
+		assert self.typed_las_prec, "Cannot compute f1 without precision score!"
+		assert self.typed_las_recall, "Cannote compute f1 without recall score!"
+		self.typed_las_f1 = pd.DataFrame(
+		 	[self.typed_las_prec, self.typed_las_recall],
+		 	index=["precision", "recall"]).fillna(0).T
+
+		self.typed_las_f1["f1"] = f1(self.typed_las_f1["recall"],
+									 self.typed_las_f1["precision"]).fillna(0).round(3)
+
+		print(self.typed_las_f1)
 
 	def _EvaluateAll(self):
 		"Runs all the evaluation metrics."
--- parser_main/evaluate_test.py
+++ parser_main/evaluate_test.py
@@ -2,6 +2,7 @@
 
 import os
 import unittest
+import pandas as pd
 from data.treebank import sentence_pb2
 from data.treebank import treebank_pb2
 from parser_main import evaluate
@@ -33,8 +34,8 @@ class EvaluateTest(unittest.TestCase):
         gold_data = _read_parser_test_data("eval_data_gold")
         test_data = _read_parser_test_data("eval_data_test")
         evaluator = evaluate.Evaluator(gold_data, test_data)
-        uas_total = evaluator._UasTotal()
-        self.assertEqual(uas_total, 85.5)
+        evaluator._UasTotal()
+        self.assertEqual(evaluator.uas_total, 85.5)
         #print("uas total {}".format(uas_total))
         print("Passed!")
 
@@ -43,10 +44,87 @@ class EvaluateTest(unittest.TestCase):
         gold_data = _read_parser_test_data("eval_data_gold")
         test_data = _read_parser_test_data("eval_data_test")
         evaluator = evaluate.Evaluator(gold_data, test_data)
-        las_total = evaluator._LasTotal()
-        self.assertEqual(las_total, 73.0)
+        evaluator._LasTotal()
+        self.assertEqual(evaluator.las_total, 73.0)
         #print("las total {}".format(las_total))
         print("Passed!")
+
+    def test_TypedLasPrec(self):
+        print("Running test_TypedLasPrec..")
+        gold_data = _read_parser_test_data("eval_data_gold")
+        test_data = _read_parser_test_data("eval_data_test")
+        evaluator = evaluate.Evaluator(gold_data, test_data)
+        evaluator._TypedLasPrec()
+        expected_result = {
+            u'subj': 1.0,
+            u'det': 1.0,
+            u'dobj': 0.67,
+            u'pobj': 1.0,
+            u'jj': 0.0,
+            u'root': 1.0,
+            u'prep': 0.0
+        }
+        self.assertDictEqual(evaluator.typed_las_prec, expected_result)
+        print("Passed!")
+
+    def test_TypedLasRecall(self):
+        print("Running test_TypedLasRecall..")
+        gold_data = _read_parser_test_data("eval_data_gold")
+        test_data = _read_parser_test_data("eval_data_test")
+        evaluator = evaluate.Evaluator(gold_data, test_data)
+        evaluator._TypedLasRecall()
+        expected_result = {
+            u'root': 1.0,
+            u'det': 0.33,
+            u'dobj': 1.0,
+            u'pobj': 1.0,
+            u'subj': 1.0,
+            u'prep': 0.0
+        }
+        self.assertDictEqual(evaluator.typed_las_recall, expected_result)
+        print("Passed!")
+
+    def test_TypedLasF1(self):
+        print("Running test_TypedLasF1..")
+        gold_data = _read_parser_test_data("eval_data_gold")
+        test_data = _read_parser_test_data("eval_data_test")
+        evaluator = evaluate.Evaluator(gold_data, test_data)
+        evaluator._TypedLasRecall()
+        evaluator._TypedLasPrec()
+        evaluator._TypedLasF1()
+        expected_prec = {
+            u"det": 1.00,
+            u"dobj":0.67,
+            u"jj":0.00,
+            u"pobj":1.00,
+            u"prep":0.00,
+            u"root":1.00,
+            u"subj":1.00,
+        }
+        expected_recall = {
+            u'root': 1.0,
+            u'det': 0.33,
+            u'dobj': 1.0,
+            u'pobj': 1.0,
+            u'subj': 1.0,
+            u'prep': 0.0,
+            u"jj": 0.00
+        }
+        expected_f1 = {
+            u"det": 0.496,
+            u"dobj":0.802,
+            u"jj":0.000,
+            u"pobj":1.000,
+            u"prep":0.000,
+            u"root":1.000,
+            u"subj":1.000
+        }
+        expected_result = pd.DataFrame([expected_prec, expected_recall, expected_f1],
+            index=["precision", "recall", "f1"]).T
+        self.assertItemsEqual(expected_result, evaluator.typed_las_f1)
+        print("Passed!")
+
+
 
 if __name__ == "__main__":
 	unittest.main()
