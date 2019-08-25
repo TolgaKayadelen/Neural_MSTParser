0e0a93ce8157d7f4a7f4cb000a1b41ecb269e224
tkayadelen <kayadelen.tolga@gmail.com>
2019-08-25 10:00:13 +0100

    [Feature Request] parse mode saves output to a file, closes: #29

--- parser_main/parse.py
+++ parser_main/parse.py
@@ -11,6 +11,7 @@ from data.treebank import treebank_pb2
 from parser.dependency_parser import DependencyParser
 from util import common
 from util import reader
+from util.writer import write_proto_as_text
 
 from google.protobuf import text_format
 
@@ -21,10 +22,10 @@ _TREEBANK_DIR = "data/UDv23"
 
 
 def parse(args):
-    """Parses a set of sentences with the loaded parsing model. 
-    
+    """Parses a set of sentences with the loaded parsing model.
+
     Args:
-        args: the command line arguments.   
+        args: the command line arguments.
     Returns:
         treebank: treebank_pb2.Treebank, a treebank of sentences parsed with the
         loaded model.
@@ -32,20 +33,20 @@ def parse(args):
     languages = {"Turkish": 1, "English": 2}
     output_treebank = treebank_pb2.Treebank()
     output_treebank.language = languages[args.language]
-    
+
     # sanity check on the command line arguments
     assert args.train_data is None, "Cannot train in parse mode!"
     assert args.load is not False, "Specify which model to use!"
     if args.model == "model.json":
         logging.info("No model specified, using the default one!")
-    
+
     # load the model.
     model = DependencyParser(decoding="mst")
     model.Load(args.model)
     logging.info("Loading model from {}".format(args.model))
     logging.info("Number of features: {}".format(model.arc_perceptron.feature_count))
     raw_input("Press any key to continue: ")
-    
+
     # read the sentences to parse into proto
     _TEST_DATA_DIR = os.path.join(_TREEBANK_DIR, args.language, "test")
     test_path = os.path.join(_TEST_DATA_DIR, "{}.pbtxt".format(args.test_data))
@@ -53,7 +54,7 @@ def parse(args):
     test_data = list(test_treebank.sentence)
     logging.info("Total sentences in test data {}".format(len(test_data)))
     #print(text_format.MessageToString(test_data[0], as_utf8=True))
-    
+
     # parse sentences with the loaded model.
     test_data = map(common.ConnectSentenceNodes, test_data)
     test_data = map(common.ExtendSentence, test_data)
@@ -64,6 +65,12 @@ def parse(args):
     print("----------")
     print("Treebank")
     print(text_format.MessageToString(output_treebank, as_utf8=True))
-    
+
+    # save the parsed sentences to an output
+    _OUTPUT_DIR = os.path.join(_TREEBANK_DIR, args.language, "parsed")
+    output_path = os.path.join(_OUTPUT_DIR, "parsed_{}_{}.pbtxt".format(args.model, args.test_data))
+    write_proto_as_text(output_treebank, output_path)
+    logging.info("{} sentences written to {}".format(len(output_treebank.sentence), output_path))
+
+
     return output_treebank
-        
