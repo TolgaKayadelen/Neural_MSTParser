2aa4e1033a1bac2d0680b8d5df1179c58c892498
TolgaKayadelen <kayadelen.tolga@gmail.com>
2019-12-21 02:24:37 +0000

    fixing model.py and adding experiment with pruned featureset

--- model/model.py
+++ model/model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""The model class provides an interface to interact with pretrained models.
+"""This module provides an interface to interact with pretrained models.
 
 A model is a json formatted dictionary which has the following keys and values:
 
@@ -10,6 +10,12 @@ accuracy: dictionary of {test_unavg: accuracy, test_avg: accuracy}.
 nr_epochs: the number of epochs used in training.
 featureset: the featureset used in training the model, in featureset_pb2 proto format.
 feature_count: the number of features in the model.
+
+Usage:
+bazel build //model:model && 
+bazel-bin/model/model --name=<model name> --print_feature_impact=<whether to print feature impact>
+--prune=<whether to prune the useless features> --output=<path to save the model after pruning>
+--write_feature_tsv=<path to write the features as a tsv file>
 
 """
 
@@ -74,7 +80,8 @@ class Model(object):
     for feat in featureset.feature:
       impact[feat.name] += abs(round(feat.weight, 2))
     impact_df = pd.DataFrame.from_dict(impact, orient='index', columns=["impact"])
-    print(impact_df.sort_values(by="impact", ascending=False, inplace=True))
+    impact_df.sort_values(by="impact", ascending=False, inplace=True)
+    print(impact_df)
     return impact
   
   def prune(self):
@@ -139,22 +146,24 @@ class Model(object):
       name: the name of the model to load.
     Returns:
       model: a json formatted dictionary.
-    """    
+    """ 
+    print("loading model from {}".format(name))
     input_file = os.path.join(_MODEL_DIR, "{}".format(name))
     with open(input_file, "r") as inp:
       model = json.load(inp)
     return model
   
-  def main(args):
-    model = Model(args.name)
-    if args.print_feature_impact:
-      model.feature_impact()
-    if args.write_feature_tsv:
-      model.write_features_as_tsv()
-    if args.prune:
-      model.prune()
-    if args.output:
-      model.save(name=args.output)
+def main(args):
+  model = Model(args.name)
+  #print(model)
+  if args.print_feature_impact:
+    model.feature_impact()
+  if args.write_feature_tsv:
+    model.write_features_as_tsv()
+  if args.prune:
+    model.prune()
+  if args.output:
+    model.save(name=args.output)
     
 
 if __name__ == "__main__":
