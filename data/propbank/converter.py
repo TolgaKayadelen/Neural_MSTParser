14a4ea9cbdbb9e467ff67c54c3651d4e94037f9e
TolgaKayadelen <kayadelen.tolga@gmail.com>
2020-01-04 02:11:23 +0300

    adding v0.5 of propbank converter script

--- data/propbank/converter.py
+++ data/propbank/converter.py
@@ -3,7 +3,8 @@
 """This module converts xml formatted propbank frames to frame.proto"""
 
 import xml.etree.ElementTree as ET
-
+from os import listdir
+from os.path import isfile, join
 
 indent = '\t'
 
@@ -17,9 +18,12 @@ def get_argument_roles(arguments):
     print("{}number: {}".format(indent*3, attr["n"]))
     print("{}suffix: {}".format(indent*3, attr["suffix"]))
     print("{}description: {}".format(indent*3, attr["descr"].encode("utf-8")))
-    vnrole,  = [child for child in argument if child.tag == "vnrole"]
-    #print(vnrole.attrib)
-    print("{}theta_role: {}".format(indent*3, vnrole.attrib["vntheta"])) 
+    vnrole =  argument.findall("vnrole")
+    if vnrole:
+      if (len(vnrole)) > 1:
+        print("this argument has more than one theta roles".format())
+        raw_input("press to continue")
+      print("{}theta_role: {}".format(indent*3, vnrole[0].attrib["vntheta"])) 
 
 
 def get_frame(roleset):
@@ -30,16 +34,26 @@ def get_frame(roleset):
 
 
 def main():
-  xmlfile = "./data/propbank/frames/gönder.xml"
-  tree = ET.parse(xmlfile)
-  root = tree.getroot()
-  predicates = [child for child in root if child.tag == "predicate"]
-  for predicate in predicates:
-    print("PREDICATE")
-    print("{}lemma: {}".format(indent, predicate.attrib["lemma"].encode("utf-8")))
-    rolesets = [child for child in predicate if child.tag == "roleset"]
-    for roleset in rolesets:
-      get_frame(roleset)
+  #files = ["gel.xml"]
+  unique_senses = 0
+  path = "./data/propbank/frames"
+  files = [f for f in listdir(path) if isfile(join(path, f))]
+  #myfiles = files[:50]
+  for file_ in files:
+    xmlfile = join(path, file_)
+    if xmlfile in ["./data/propbank/frames/framesetTR.dtd", "./data/propbank/frames/frameset.dtd"]:
+      continue
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    predicates = [child for child in root if child.tag == "predicate"]
+    for predicate in predicates:
+      print("PREDICATE")
+      print("{}lemma: {}".format(indent, predicate.attrib["lemma"].encode("utf-8")))
+      rolesets = [child for child in predicate if child.tag == "roleset"]
+      for roleset in rolesets:
+        unique_senses += 1
+        get_frame(roleset)
+  print("total verb senses: {}".format(unique_senses))
 
 
 if __name__ == "__main__":
