aae38f06620b2cf1b6e89179ccad1b0fc734f555
TolgaKayadelen <kayadelen.tolga@gmail.com>
2020-01-04 22:52:06 +0300

    adding propbank converter v1.0 and the converted frame files

--- data/propbank/converter.py
+++ data/propbank/converter.py
@@ -2,58 +2,111 @@
 
 """This module converts xml formatted propbank frames to frame.proto"""
 
+import sys
+import itertools
 import xml.etree.ElementTree as ET
 from os import listdir
 from os.path import isfile, join
+from util import writer
 
-indent = '\t'
+from data.propbank.protos import frame_pb2
+from google.protobuf import text_format
 
-def get_argument_roles(arguments):
-  for argument in arguments:
-    if argument.tag == "note":
-      continue
-    attr = argument.attrib
-    #print(attr)
-    print("{}argument".format(indent*2))
-    print("{}number: {}".format(indent*3, attr["n"]))
-    print("{}suffix: {}".format(indent*3, attr["suffix"]))
-    print("{}description: {}".format(indent*3, attr["descr"].encode("utf-8")))
-    vnrole =  argument.findall("vnrole")
-    if vnrole:
-      if (len(vnrole)) > 1:
-        print("this argument has more than one theta roles".format())
-        raw_input("press to continue")
-      print("{}theta_role: {}".format(indent*3, vnrole[0].attrib["vntheta"])) 
+_INPUT_FRAMES_DIR = "./data/propbank/frames_xml"
+_OUTPUT_FRAMES_DIR = "./data/propbank/frames_proto"
 
+def get_argnum_enum(n):
+  if n in ["0", "1", "2", "3", "4", "5", "6"]:
+    argnum = frame_pb2.ArgNum = int(n) + 1
+  elif n == "m":
+    argnum = frame_pb2.A_LVB
+  else:
+    argnum = frame_pb2.UNKNOWN_ARG
+  return argnum
 
-def get_frame(roleset):
-  print("{}id: {}".format(indent*2, roleset.attrib["id"].encode("utf-8")))
-  print("{}sense: {}".format(indent*2, roleset.attrib["name"].encode("utf-8")))
-  arguments = [child for child in roleset if child.tag == "roles"]
-  map(get_argument_roles, arguments)
+def get_suffix_enum(suffix):
+  if suffix == "NOM":
+    return frame_pb2.NOM
+  elif suffix == "ACC":
+    return frame_pb2.ACC
+  elif suffix == "DAT":
+    return frame_pb2.DAT
+  elif suffix == "LOC":
+    return frame_pb2.LOC
+  elif suffix == "ABL":
+    return frame_pb2.ABL
+  elif suffix == "INS":
+    return frame_pb2.INS
+  elif suffix == "WITH":
+    return frame_pb2.INS
+  elif suffix == "NONE" or suffix == "":
+    return frame_pb2.UNKNOWN_CASE
+  else:
+    sys.exit("Couldn't find a value for this suffix in the proto {}".format(suffix))
 
+def populate_argument_structure(roleset, arg_str):
+  arg_str.id = roleset.attrib["id"]
+  arg_str.sense = roleset.attrib["name"]  
+  def populate_argument_roles(roles):
+    for role in roles:
+      if role.tag == "note":
+        continue
+      argument = arg_str.argument.add()
+      argument.description = role.attrib["descr"]
+      argument.number = get_argnum_enum(role.attrib["n"])
+      suffix = role.attrib["suffix"].split("-")
+      argument.case.extend(map(get_suffix_enum, suffix))
+      vnrole = role.findall("vnrole")
+      if vnrole:
+        if len(vnrole) > 1:
+          print("This argument has more than one theta roles, taking the first one")
+          raw_input("Press to continue")
+        argument.theta_role = vnrole[0].attrib["vntheta"]
+  
+  roles = [child for child in roleset if child.tag == "roles"]
+  examples = [child for child in roleset if child.tag == "example"]
+  for example in examples:
+    for child in example:
+      if child.tag == "text":
+        arg_str.example = child.text if child.text else "None"
+        break
+  map(populate_argument_roles, roles)
+  return arg_str
 
-def main():
-  #files = ["gel.xml"]
+def xml_to_proto(files):
+  frames = []
   unique_senses = 0
-  path = "./data/propbank/frames"
-  files = [f for f in listdir(path) if isfile(join(path, f))]
-  #myfiles = files[:50]
   for file_ in files:
-    xmlfile = join(path, file_)
-    if xmlfile in ["./data/propbank/frames/framesetTR.dtd", "./data/propbank/frames/frameset.dtd"]:
+    if file_ in ["framesetTR.dtd", "frameset.dtd"]:
       continue
+    print(file_)
+    xmlfile = join(_INPUT_FRAMES_DIR, file_)
     tree = ET.parse(xmlfile)
     root = tree.getroot()
+    frame = frame_pb2.Frame()
+    frame.frame_id = file_.split(".")[0]
     predicates = [child for child in root if child.tag == "predicate"]
     for predicate in predicates:
-      print("PREDICATE")
-      print("{}lemma: {}".format(indent, predicate.attrib["lemma"].encode("utf-8")))
+      #verb = frame_pb2.Verb()
+      verb = frame.verb.add()
+      verb.lemma = predicate.attrib["lemma"]
       rolesets = [child for child in predicate if child.tag == "roleset"]
       for roleset in rolesets:
         unique_senses += 1
-        get_frame(roleset)
-  print("total verb senses: {}".format(unique_senses))
+        arg_str = verb.argument_structure.add()
+        populate_argument_structure(roleset, arg_str)
+    frames.append(frame)
+  return frames
+
+def main():
+  #files = ["gel.xml", "getir.xml"]
+  files = [f for f in listdir(_INPUT_FRAMES_DIR) if isfile(join(_INPUT_FRAMES_DIR, f))]
+  frames = xml_to_proto(files)
+  for frame in frames:
+    writer.write_proto_as_text(
+      frame, join(_OUTPUT_FRAMES_DIR, "{}.pbtxt".format(frame.frame_id.encode("utf-8"))))
+    #print(text_format.MessageToString(verb, as_utf8=True))
+    
 
 
 if __name__ == "__main__":
