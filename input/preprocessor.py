433426f7ce69d7ca10105779f42846e559223955
TolgaKayadelen <kayadelen.tolga@gmail.com>
2021-01-06 15:55:23 +0300

    start implementing the input pipeline, implemented the embeddor, #58

---
+++ input/preprocessor.py
@@ -0,0 +1,122 @@
+import numpy as np
+import random
+from enum import Enum
+from typing import List, Dict
+from util.nn import nn_utils
+
+Array = np.ndarray
+
+class SanityCheck(Enum):
+  UNKNOWN = 1
+  PASS = 2
+  FAIL = 3
+
+class Embeddor:
+  def __init__(self, *, name:str, matrix):
+    self.name = name
+    self.vocab = matrix
+    self.vocab_size = len(self.vocab)
+    self.embedding_dim = matrix
+    self.token_to_index = self._token_to_index()
+    self.index_to_token = self._index_to_token()
+    self.token_to_vector = matrix
+    self.index_to_vector = self._index_to_vector(matrix=matrix)
+  
+  def __str__(self):
+    return f"""
+      {self.name} embeddings, dimension: {self.dimension}, vocab_size: {self.vocab_size}
+      """
+
+  def _token_to_index(self) -> Dict[int, int]:
+    return {elem:ind for ind, elem in enumerate(self.vocab)}
+  
+  def _index_to_token(self) -> Dict[int, int]:
+    return {ind:elem for ind, elem in enumerate(self.vocab)}       
+
+  def _index_to_vector(self, *, matrix) -> Array:
+    index_to_vector = np.zeros(shape=(self.vocab_size, self.embedding_dim))
+    for token, idx in self.token_to_index.items():
+      # print(token, idx)
+      if token == "-pad-":
+        index_to_vector[idx, :] = np.zeros(shape=(self.embedding_dim,))
+      elif token == "-oov-":
+        # randomly initialize
+        index_to_vector[idx, :] = np.random.randn(self.embedding_dim,)
+      else:
+        index_to_vector[idx, :] = matrix[token]
+        # print(matrix[token])
+    return index_to_vector
+  
+  # Accessor methods for individual items are defined here.
+  def stoi(self, *, token: str) -> int:
+    return self.token_to_index[token]
+  
+  def itos(self, *, idx: int) -> str:
+    return self.index_to_token[idx]
+  
+  def ttov(self, *, token: str):
+    return self.token_to_vector[token]
+  
+  def itov(self, *, idx: int):
+    return self.index_to_vector[idx]
+  
+  # Embedding properties are set based on the properties of the 
+  # word2vec matrix.
+  @property
+  def vocab(self):
+    return self._vocab
+  
+  @vocab.setter
+  def vocab(self, matrix) -> List:
+    "Setting up the vocabulary."
+    print("Setting up the vocabulary")
+    vocab = []
+    vocab.extend(["-pad-", "-oov-"])
+    vocab.extend(sorted(set(matrix.wv.vocab.keys())))
+    self._vocab = vocab
+  
+  @property
+  def embedding_dim(self):
+    return self._embedding_dim
+  
+  @embedding_dim.setter
+  def embedding_dim(self, matrix):
+    print("Setting embedding dimension")
+    random_token = random.choice(self.vocab)
+    self._embedding_dim = matrix[random_token].shape[0]
+  
+  @property
+  def sanity_check(self) -> SanityCheck:
+    print("Running sanity checks on the created embeddings")
+    random_token = random.choice(self.vocab)
+    random_tok_idx = self.stoi(token=random_token)
+    if not len(self.token_to_vector.wv.vocab.keys()) == self.vocab_size - 2:
+      return SanityCheck.FAIL
+    if not self.vocab[0] == "-pad-" and self.vocab[1] == "-oov-":
+      return SanityCheck.FAIL
+    if not self.stoi(token=random_token) == self.token_to_index[random_token]:
+      return SanityCheck.FAIL
+    if not self.itos(idx=random_tok_idx) == self.index_to_token[random_tok_idx]:
+      return SanityCheck.FAIL
+    if not (self.token_to_vector[random_token] == self.index_to_vector[random_tok_idx]).all():
+      return SanityCheck.FAIL
+    if not (self.itov(idx=random_tok_idx) == self.index_to_vector[random_tok_idx]).all():
+      return SanityCheck.FAIL
+    if not (self.token_to_vector[self.itos(idx=random_tok_idx)] == self.token_to_vector[
+      random_token]).all():
+      return SanityCheck.FAIL
+    if not (self.index_to_vector[self.stoi(token=random_token)] == self.token_to_vector[
+      random_token]).all():
+      return SanityCheck.FAIL
+    return SanityCheck.PASS
+    
+
+if __name__ == "__main__":
+  embeddings = nn_utils.load_embeddings()
+  myemb = Embeddor(name="word2vec", matrix=embeddings)
+  print(myemb.name)
+  # print(myemb.vocab)
+  print(myemb.vocab_size)
+  print(myemb.embedding_dim)
+  # print(myemb.itos(idx=493047))
+  print(myemb.sanity_check)
